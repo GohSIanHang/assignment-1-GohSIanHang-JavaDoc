b63416900ca0428b706862880a783fc2a270ff52
Goh Sian Hang <72654459+GohSIanHang@users.noreply.github.com>
2020-11-16 11:10:06 +0800

    Add files via upload
    initial commit

--- package-search-index.js
+++ package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.GSH"},{"l":"my.example"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"my.assignment1"},{"l":"my.example"}];updateSearchResults();
